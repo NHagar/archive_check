329e0dabc5bd5e3c3d316f562a6603fe741f51e1
NHagar <nicholasrhagar@gmail.com>
2021-05-25 21:23:11 -0500

    reformatting for regression

--- latimes_analysis.py
+++ latimes_analysis.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 # %%
 con = sqlite3.connect("./data/lat.db")
-df = pd.read_sql_query("SELECT * FROM parsed_articles", con)
+df = pd.read_sql_query("SELECT url, url_canonical, hed, pub_date FROM parsed_articles", con)
 pos = pd.read_sql_query("SELECT * FROM partsofspeech", con)
 services = pd.read_csv("./data/lat_all_urls.csv")
 novdf = services[(services['url'].str.contains("/story/")) &
@@ -19,6 +19,22 @@ biden_urls = biden_urls - both
 # %%
 pos_trump = pos[pos['url'].isin(trump_urls)]
 pos_biden = pos[pos['url'].isin(biden_urls)]
-# %%
 
+# %%
+def reformat(df):
+    df['value'] = 1
+    pv = df.pivot_table(index='url', columns='pos', values='value')
+    return pv
+
+# %%
+pos_trump = reformat(pos_trump)
+pos_biden = reformat(pos_biden)
+# %%
+pos_trump.loc[:, 'candidate'] = 0
+pos_biden.loc[:, 'candidate'] = 1
+pos_all = pos_trump.append(pos_biden).fillna(0)
+
+# %%
+X = pos_all.drop(columns=['candidate']).to_numpy()
+y = pos_all.candidate.to_numpy()
 # %%
