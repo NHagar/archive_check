097e00075b719700cdd94bc8edaee73301cf95de
NHagar <nicholasrhagar@gmail.com>
2021-06-10 17:14:55 -0500

    reorganizing processing steps

--- compare_results.py
+++ compare_results.py
@@ -8,21 +8,15 @@ RESULTS_PATH = pathlib.Path("./results")
 databases = list(DATA_PATH.glob("*.db"))
 
 for d in databases:
-    # Load and preprocess
+    # Load and set up file structure
     dpath = RESULTS_PATH / d
     dpath.mkdir(parents=True, exist_ok=True)
-    data = db.Database(d)
+    con = sqlite3.connect(d)
+    data = db.Database(con)
     tables = [i for i in data.list_tables() if i!="parsed_articles"]
-    tables_cleaned = [data.join_to_parsed(t) for t in tables]
-    tables_cleaned = [analysis.clean_dataframe(t) for t in tables_cleaned]
-    
-    # Clean tables
-    for t in tables:
-        df = data.join_to_parsed(t)
-        df_cleaned = analysis.clean_dataframe(df)
-        # URL count
-        urls = len(df_cleaned.url_canonical.unique())
-        
+    tables_cleaned = [(t, data.join_to_parsed(t)) for t in tables]
+    tables_cleaned = [analysis.Table(*t) for t in tables_cleaned]
+
 
 # LDA
 
--- pipeline/analysis.py
+++ pipeline/analysis.py
@@ -1,15 +1,28 @@
+from dataclasses import dataclass
+
 import pandas as pd
 
+@dataclass
+class Table:
+    name: str
+    df: pd.DataFrame
 
-def clean_dataframe(df: pd.DataFrame, start_date: str, end_date: str) -> pd.DataFrame:
-    """Common analysis preprocessing steps
-    """
-    start = pd.to_datetime(start_date)
-    end = pd.to_datetime(end_date)
-    # Get rid of failed joins
-    df = df[(~df.text.isna()) & (~df.pub_date.isna())]
-    # Convert and filter datetime
-    df.loc[:, "pub_date"] = pd.to_datetime(df.pub_date, utc=True)
-    df = df[(df.pub_date>=start) & (df.pub_date<=end)]
+    def clean_dataframe(self, start_date: str, end_date: str) -> pd.DataFrame:
+        """Common analysis preprocessing steps
+        """
+        start = pd.to_datetime(start_date)
+        end = pd.to_datetime(end_date)
+        # Get rid of failed joins
+        df = self.df[(~self.df.text.isna()) & (~self.df.pub_date.isna())]
+        # Convert and filter datetime
+        df.loc[:, "pub_date"] = pd.to_datetime(df.pub_date, utc=True)
+        df = df[(df.pub_date>=start) & (df.pub_date<=end)]
 
-    return df
+        return df
+
+    def count_urls(df: pd.DataFrame) -> int:
+        """Count unique URLs in dataframe
+        """
+        urls = len(df.url_canonical.unique())
+
+        return urls
