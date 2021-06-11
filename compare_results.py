0216ea28dcd7443f936f413581a982861e0d0ccd
NHagar <nicholasrhagar@gmail.com>
2021-06-10 19:23:15 -0500

    uncommenting

--- compare_results.py
+++ compare_results.py
@@ -20,22 +20,22 @@ for d in databases:
     tables = [i for i in data.list_tables() if i!="parsed_articles"]
     tables_cleaned = [(t, data.join_to_parsed_clean(t, "2020-11-01", "2020-11-30")) for t in tables]
     tables_cleaned = [analysis.Table(*t) for t in tables_cleaned]
-    # # URL counts
-    # url_counts = [{"service": i.name, "urls": i.count_urls()} for i in tables_cleaned]
-    # url_counts = pd.DataFrame(url_counts)
-    # url_counts.to_csv(dpath / "urlcounts.csv", index=False)
-    # # LDA
-    # # NLP preprocessing
-    # nlp = analysis.init_spacy(["advertisement", "Advertisement", "said", "Said"],
-    #                           ['tok2vec', 'tagger', 'parser', 'ner', 'attribute_ruler', 'lemmatizer'])
-    # for t in tables_cleaned:
-    #     t.process_body(nlp)
-    #     t.build_corpus()
-    #     t.train_models(20)
-    # best_models = [t.get_best_model() for t in tables_cleaned]
-    # best_models = pd.DataFrame(best_models)
-    # best_models.to_csv(dpath / "lda.csv", index=False)
-    # Headline analysis
+    # URL counts
+    url_counts = [{"service": i.name, "urls": i.count_urls()} for i in tables_cleaned]
+    url_counts = pd.DataFrame(url_counts)
+    url_counts.to_csv(dpath / "urlcounts.csv", index=False)
+    # LDA
+    # NLP preprocessing
+    nlp = analysis.init_spacy(["advertisement", "Advertisement", "said", "Said"],
+                              ['tok2vec', 'tagger', 'parser', 'ner', 'attribute_ruler', 'lemmatizer'])
+    for t in tables_cleaned:
+        t.process_body(nlp)
+        t.build_corpus()
+        t.train_models(20)
+    best_models = [t.get_best_model() for t in tables_cleaned]
+    best_models = pd.DataFrame(best_models)
+    best_models.to_csv(dpath / "lda.csv", index=False)
+    Headline analysis
     nlp = analysis.init_spacy([], [])
     for t in tables_cleaned:
         t.process_hed(nlp)
