a3e61d6a3012475c20394d1f1ec9cb119738cce5
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2022-07-17 12:18:13 -0700

    updates old onsite scripts

--- scripts/vox_onsite.py
+++ scripts/vox_onsite.py
@@ -6,8 +6,11 @@ import pyppeteer
 
 con = sqlite3.connect("./data/vox.db")
 
-index_page = "https://www.vox.com/archives/2020/11"
+years = [2015, 2020]
+index_pages = [f"https://www.vox.com/archives/{y}/11" for y in years]
+
 BUTTON_SELECT = "button.c-archives-load-more__button"
+results = []
 
 async def autoScroll(page):
     await page.evaluate('''async () => {
@@ -32,28 +35,30 @@ async def main():
     browser = await pyppeteer.launch({"headless": False})
     page = await browser.newPage()
     await page.setViewport({"width": 900, "height": 1080})
-    await page.goto(index_page)
-    await page.waitFor(3000)
-    await autoScroll(page)
-    # Find and click "load more" until it disappears
-    button = await page.querySelector(BUTTON_SELECT)
-    while button:
-        await page.focus(BUTTON_SELECT)
-        await page.click(BUTTON_SELECT)
-        await page.waitFor(5000)
+    for index_page in index_pages:
+        await page.goto(index_page)
+        await page.waitFor(3000)
         await autoScroll(page)
+        # Find and click "load more" until it disappears
         button = await page.querySelector(BUTTON_SELECT)
-    # Save all URLs on full page    
-    urls = await page.evaluate('''() => {
-            var links = document.querySelectorAll("h2.c-entry-box--compact__title > a");
-            var links_array = [...links];
-            var urls = links_array.map(function(elem) {return elem.getAttribute("href")});
+        while button:
+            await page.focus(BUTTON_SELECT)
+            await page.click(BUTTON_SELECT)
+            await page.waitFor(5000)
+            await autoScroll(page)
+            button = await page.querySelector(BUTTON_SELECT)
+        # Save all URLs on full page    
+        urls = await page.evaluate('''() => {
+                var links = document.querySelectorAll("h2.c-entry-box--compact__title > a");
+                var links_array = [...links];
+                var urls = links_array.map(function(elem) {return elem.getAttribute("href")});
 
-            return urls;
-        }
-    ''')
+                return urls;
+            }
+        ''')
+        results.extend(urls)
 
-    urls_series = pd.Series(urls, name="url")
+    urls_series = pd.Series(results, name="url")
     urls_series.to_sql("onsite", con)
 
     await browser.close()
