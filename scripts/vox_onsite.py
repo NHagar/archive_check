49b6d24c22503e962c63bbadcb1b52b613004d97
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2022-07-17 15:17:54 -0700

    updates old onsite scripts

--- scripts/vox_onsite.py
+++ scripts/vox_onsite.py
@@ -2,7 +2,7 @@ import asyncio
 import sqlite3
 
 import pandas as pd
-import pyppeteer
+from playwright.async_api import async_playwright
 
 con = sqlite3.connect("./data/vox.db")
 
@@ -32,34 +32,35 @@ async def autoScroll(page):
 
 
 async def main():
-    browser = await pyppeteer.launch({"headless": False})
-    page = await browser.newPage()
-    await page.setViewport({"width": 900, "height": 1080})
-    for index_page in index_pages:
-        await page.goto(index_page)
-        await page.waitFor(3000)
-        await autoScroll(page)
-        # Find and click "load more" until it disappears
-        button = await page.querySelector(BUTTON_SELECT)
-        while button:
-            await page.focus(BUTTON_SELECT)
-            await page.click(BUTTON_SELECT)
-            await page.waitFor(5000)
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        page = await browser.new_page()
+        await page.set_viewport_size({"width": 900, "height": 1080})
+        for index_page in index_pages:
+            await page.goto(index_page)
+            await page.wait_for_timeout(3000)
             await autoScroll(page)
-            button = await page.querySelector(BUTTON_SELECT)
-        # Save all URLs on full page    
-        urls = await page.evaluate('''() => {
-                var links = document.querySelectorAll("h2.c-entry-box--compact__title > a");
-                var links_array = [...links];
-                var urls = links_array.map(function(elem) {return elem.getAttribute("href")});
+            # Find and click "load more" until it disappears
+            button = await page.query_selector(BUTTON_SELECT)
+            while button:
+                await page.focus(BUTTON_SELECT)
+                await page.click(BUTTON_SELECT)
+                await page.wait_for_timeout(5000)
+                await autoScroll(page)
+                button = await page.query_selector(BUTTON_SELECT)
+            # Save all URLs on full page    
+            urls = await page.evaluate('''() => {
+                    var links = document.querySelectorAll("h2.c-entry-box--compact__title > a");
+                    var links_array = [...links];
+                    var urls = links_array.map(function(elem) {return elem.getAttribute("href")});
 
-                return urls;
-            }
-        ''')
-        results.extend(urls)
+                    return urls;
+                }
+            ''')
+            results.extend(urls)
 
     urls_series = pd.Series(results, name="url")
-    urls_series.to_sql("onsite", con)
+    urls_series.to_sql("onsite", con, if_exists="replace")
 
     await browser.close()
 
